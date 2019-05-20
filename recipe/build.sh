cf39abb2526c00781650c51d3f3c14e6d6807b20
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-20 18:42:22 -0300

    qt GXX workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,11 @@
 [[ -d build ]] || mkdir build
 cd build/
 
+# Missing g++ workaround.
+ln -s ${GXX} g++ || true
+chmod +x g++
+export PATH=${PWD}:${PATH}
+
 cmake \
     -D CMAKE_INSTALL_PREFIX=$PREFIX \
     $SRC_DIR
