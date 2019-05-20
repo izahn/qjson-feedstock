46da973c0fc9a02350b1a1b79b61acb5055be8b0
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-20 19:04:03 -0300

    add enable_language.patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,9 @@
 [[ -d build ]] || mkdir build
 cd build/
 
-# Missing g++ workaround.
-ln -s ${GXX} g++ || true
-chmod +x g++
-export PATH=${PWD}:${PATH}
-
 cmake \
-    -D CMAKE_INSTALL_PREFIX=$PREFIX \
-    $SRC_DIR
+    -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+    ${SRC_DIR}
 
 make
 # No "make check" available
